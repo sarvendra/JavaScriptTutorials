c1889a18f71dda1ed8a84455088f6cea46f0c940
sarvendra <sarvendraanand@gmail.com>
2016-01-19 15:07:06 +0530

    Add solution to chapter11 exercises

---
+++ EloquentJavascriptExcercises/Chapter11/arrays.js
@@ -0,0 +1,7 @@
+topEnv["array"] = function(){
+  return Array.prototype.slice.call(arguments, 0);;
+};
+
+topEnv["length"] = function(array){
+  return array.length;
+};
---
+++ EloquentJavascriptExcercises/Chapter11/fixingScope.js
@@ -0,0 +1,14 @@
+specialForms["set"] = function(args, env) {
+  if (args.length != 2 || args[0].type != "word")
+    throw new SyntaxError("Bad use of set");
+  var name = args[0].name;
+  var value = evaluate(args[1], env);
+
+  for (var scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+    if (Object.prototype.hasOwnProperty.call(scope, name)) {
+      scope[name] = value;
+      return value;
+    }
+  }
+  throw new ReferenceError(name + " is undefined");
+};
